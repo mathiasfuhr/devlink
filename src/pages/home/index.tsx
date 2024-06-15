ec2bc03cccd2a3f2861a68f7113f149e529b144d
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-06-15 18:26:44 -0300

    add tailwindcss

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
-export function Home(){
-    return(
-        <div>
-            <h1>pagina Home</h1>
-        </div>
-    )
+export function Home() {
+  return (
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Mathias Fuhr</h1>
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12">
+        <section>
+            <a>
+                <p>
+                    Linkedin
+                </p>
+            </a>
+        </section>
+      </main>
+    </div>
+  );
 }
